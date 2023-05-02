c5d61047bc897ce38ad7403ceb942802a240c10a
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-05-02 17:25:59 +0200

    stampa titolo in pagina

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+  export default{
+        name: "AppTitle",
+    }
+</script>
+
+<template>
+  <div class="container text-center">
+    <h1>La mia prima app con Vite!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
